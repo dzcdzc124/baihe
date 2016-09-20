55a18494bc8363b643b4ef432b4336baf7962d38
dzcdzc124 <dengzhengcai@vivo.com.cn>
2016-09-20 15:41:11 +0800

    no message

--- app/core/Lib/Mvc/Dispatcher.php
+++ app/core/Lib/Mvc/Dispatcher.php
@@ -9,6 +9,8 @@ class Dispatcher extends PhDispatcher
 {
     public function __construct()
     {
+        var_dump(get_class_methods(new parent));
+        die();
         parent::__construct();
 
         $eventManager = new PhEventsManager();
